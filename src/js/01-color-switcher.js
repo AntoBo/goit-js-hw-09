056a11f51b0f7aa6bd5ae6ec26198ebaafef8ccc
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-13 11:42:36 +0200

    task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,41 @@
+import '../css/common.css';
 
+//get controls
+const bodyEl = document.querySelector('body');
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+refs.stopBtn.disabled = true;
+let timerId = null;
+const interval = 1000;
+
+//toogle buttons activity
+function toggleBtns(...buttons) {
+  buttons.forEach(button =>
+    button.disabled ? (button.disabled = false) : (button.disabled = true),
+  );
+}
+
+//listen buttons clicks
+refs.startBtn.addEventListener('click', () => {
+  toggleBtns(refs.stopBtn, refs.startBtn);
+
+  //switch color on first click without delay
+  bodyEl.style.backgroundColor = getRandomHexColor();
+
+  //switch color with interval
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, interval);
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  toggleBtns(refs.stopBtn, refs.startBtn);
+  clearInterval(timerId);
+});
+
+//get random color
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
