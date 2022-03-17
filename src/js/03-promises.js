8ae2dff68c821ad90fb0722acb824581966c7f62
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-17 21:26:35 +0200

    task 3 logic done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,68 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+// function createPromise() {
+//   console.log('creating promise');
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log('setTimeout');
+
+//       const shouldResolve = Math.random() > 0.5;
+//       console.log(`shouldResolve is ${shouldResolve}`);
+//       if (shouldResolve) {
+//         resolve('promise resolved');
+//       } else {
+//         reject('promise rejected');
+//       }
+//     }, 1000);
+//   });
+// }
+
+// createPromise()
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => console.log(err));
+
+let delay = 500;
+let position = 1;
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.5;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+for (let i = 0; i < 5; i++) {
+  createPromise(position, delay)
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+        timeout: 5000,
+        position: 'center-right',
+      });
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+        timeout: 5000,
+        position: 'center-right',
+      });
+    });
+
+  position += 1;
+  delay += 500;
 }
